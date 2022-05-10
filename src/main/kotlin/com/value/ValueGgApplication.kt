7ea5e371ef478dd16a98ec9c1db5222e7ef93388
Patrick Ryoo <donghoon149@gmail.com>
2022-05-10 14:54:25 +0900

    initial setting

---
+++ src/main/kotlin/com/value/ValueGgApplication.kt
@@ -0,0 +1,11 @@
+package com.value
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValueGgApplication
+
+fun main(args: Array<String>) {
+    runApplication<ValueGgApplication>(*args)
+}
